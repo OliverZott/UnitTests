0f75ceda9a517ab4d6c7d2646e90b046d5c670f1
Oliver Zott <zott_oliver@web.de>
2021-08-25 19:40:28 +0200

    Added example for Test-Setup and TestCases.

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [TestCase(2, 1, 2)]
+        [TestCase(-3, 1, 1)]
+        [TestCase(-2, -2, -2)]
+        public void Max_WhenCalled_ReturnGreaterArgument(int a, int b, int expected)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}
