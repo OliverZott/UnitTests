9df8613b6fbd752a5761bf601f63ed838027784b
Oliver Zott <zott_oliver@web.de>
2021-08-31 19:00:46 +0200

    Added test for exceptions and void-methods.

---
+++ TestNinja.UnitTests/ErrorLoggerTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTest
+    {
+
+        // Test void-methods
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        // Test methods that throw exceptions
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+    }
+}
