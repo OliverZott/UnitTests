c9e9fca8d2ee9ec38551d4466c180af3f3c40796
Oliver Zott <oliver.zott@vertigis.com>
2021-09-21 16:58:01 +0200

    Added interaction-test with faked storage.

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    /// <summary>
+    /// Interaction-test example. 
+    /// 
+    /// Ensure that right method gets called with right argument.
+    /// </summary>
+    [TestFixture]
+    internal class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoresTheOrder()
+        {
+            var order = new Order();
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+
+            orderService.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
