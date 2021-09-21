737677a3f5e2e41ca499500a2f8e6c01975e58a3
Oliver Zott <oliver.zott@vertigis.com>
2021-09-21 13:17:52 +0200

    WIP - added executable test program.

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestNinja.Mocking
+{
+    internal class Program
+    {
+
+        public static void Main()
+        {
+            var videoService = new VideoService();
+
+            // In reality a DI-Framework is used instead of "new"íng object!
+            var title = videoService.ReadVideoTitle("video.txt");
+            System.Console.WriteLine(title);
+        }
+    }
+}
