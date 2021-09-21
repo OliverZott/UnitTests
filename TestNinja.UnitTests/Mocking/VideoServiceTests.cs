70059ef5243de6b2d8a3ab57a0b96f6b0aa519a9
Oliver Zott <oliver.zott@vertigis.com>
2021-09-21 16:34:05 +0200

    Added mocking example.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,26 +1,46 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
+    /// <summary>
+    /// Mocking example with Moq.
+    /// </summary>
     [TestFixture]
     internal class VideoServiceTests
     {
 
+
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
         {
-            var videoService = new VideoService(new FakeFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            var result = videoService.ReadVideoTitle("video.txt");
+            var result = _videoService.ReadVideoTitle("video.txt");
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
 
+        [Test]
+        public void ReadVideoTitle_EmptyJsonArgument_ReturnRespectiveTitle()
+        {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("{}");
+
+            var result = _videoService.ReadVideoTitle("video.txt");
+
+            Assert.That(result, Is.EqualTo(null));
         }
     }
 }
