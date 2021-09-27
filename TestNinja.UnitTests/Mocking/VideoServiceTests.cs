e2f06a5f597cfd22a739a6aa6d936d3c43c49e6d
Oliver Zott <oliver.zott@vertigis.com>
2021-09-27 17:57:15 +0200

    Example added.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,25 +1,40 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
     /// <summary>
     /// Mocking example with Moq.
+    /// 
     /// </summary>
     [TestFixture]
     internal class VideoServiceTests
     {
 
-
         private VideoService _videoService;
         private Mock<IFileReader> _fileReader;
+        private Mock<IVideoRepository> _videoRepository;
 
+
+
+        /// REMARKS: 
+        /// 
+        /// If there are too many mocks in the [SetUp] it could mean:
+        ///     1. Class does too much and shpuld be split up!
+        ///     2. We are mocking too much!
+        ///     
+        /// If dependencies (e.g. _fileReader) is only used in one method!  -->
+        ///     --> Maybe constructor injection is not necessary!! Use parameter-injection instead.
+        /// 
+        ///     
         [SetUp]
         public void SetUp()
         {
             _fileReader = new Mock<IFileReader>();
-            _videoService = new VideoService(_fileReader.Object);
+            _videoRepository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(_fileReader.Object, _videoRepository.Object);
         }
 
 
@@ -33,6 +48,7 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
 
+
         [Test]
         public void ReadVideoTitle_EmptyJsonArgument_ReturnRespectiveTitle()
         {
@@ -41,6 +57,35 @@ namespace TestNinja.UnitTests.Mocking
             var result = _videoService.ReadVideoTitle("video.txt");
 
             Assert.That(result, Is.EqualTo(null));
+        }
+
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnsEmptyString()
+        {
+            _videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AFewUnprocessedVideos_ReturnsStringWithIdOfUnprocessedVideos()
+        {
+            var videos = new List<Video>()
+            {
+                new Video() { Id = 1},
+                new Video() { Id = 2},
+                new Video() { Id = 3}
+            };
+
+            _videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(videos);
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("1,2,3"));
         }
     }
 }
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -2,27 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
 
 namespace TestNinja.Mocking
 {
     public class VideoService
     {
         private readonly IFileReader _fileReader;
+        private readonly IVideoRepository _videoRepository;
 
         // DI via:
         //   - Parameter
         //   - Propertie
         //   - Constructor
 
-        public VideoService(IFileReader fileReader = null)
+        public VideoService(IFileReader fileReader = null, IVideoRepository videoRepository = null)
         {
             _fileReader = fileReader ?? new FileReader();
+            _videoRepository = videoRepository ?? new VideoRepository();
         }
 
         public string ReadVideoTitle(string path)
         {
-            var str = _fileReader.Read(path);
+            var str = _fileReader.Read(path);  // Decouple via DI of interface in constructor.
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
@@ -33,18 +34,12 @@ namespace TestNinja.Mocking
         {
             var videoIds = new List<int>();
 
-            using (var context = new VideoContext())
-            {
-                var videos =
-                    (from video in context.Videos
-                     where !video.IsProcessed
-                     select video).ToList();
+            var videos = _videoRepository.GetUnprocessedVideos();  // Decouple via DI of interface in constructor.
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
 
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
+            return String.Join(",", videoIds);
 
-                return String.Join(",", videoIds);
-            }
         }
     }
 
